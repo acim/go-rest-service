1f1f0d98755698c23900c00896f3c20f0785c190
Boban Acimovic <boban.acimovic@gmail.com>
2019-11-05 23:44:46 +0100

    add constraints

--- .docker/user.sql
+++ .docker/user.sql
@@ -1,5 +1,5 @@
 CREATE TABLE "user" (
-  "id" uuid NOT NULL,
-  "email" character varying(254) NOT NULL,
+  "id" uuid PRIMARY KEY,
+  "email" character varying(254) UNIQUE NOT NULL,
   "password" character(60) NOT NULL
 );
