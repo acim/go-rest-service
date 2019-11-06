58ff2809927cb103e4c632a92c381b7faddeb539
Boban Acimovic <boban.acimovic@gmail.com>
2019-11-06 03:38:25 +0100

    validate email and password

--- pkg/cmd/user.go
+++ pkg/cmd/user.go
@@ -2,11 +2,16 @@ package cmd
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"regexp"
 
 	"github.com/acim/go-rest-server/pkg/model"
 	"github.com/acim/go-rest-server/pkg/store"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
+
+const emailRegexString = "^(((([a-zA-Z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])+(\\.([a-zA-Z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])+)*)|((\\x22)((((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(([\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x7f]|\\x21|[\\x23-\\x5b]|[\\x5d-\\x7e]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(\\([\\x01-\\x09\\x0b\\x0c\\x0d-\\x7f]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}]))))*(((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(\\x22)))@((([a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(([a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])([a-zA-Z]|\\d|-|\\.|_|~|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])*([a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])))\\.)+(([a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(([a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])([a-zA-Z]|\\d|-|_|~|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])*([a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])))\\.?$"
 
 // NewUserCmd create new user command that can manipulate with the users in database.
 func NewUserCmd(app *kingpin.Application, users store.Users) *kingpin.CmdClause {
@@ -15,10 +20,24 @@ func NewUserCmd(app *kingpin.Application, users store.Users) *kingpin.CmdClause 
 	email := add.Arg("email", "user's email address").Required().String()
 	password := add.Arg("password", "user's password").Required().String()
 	add.Action(func(c *kingpin.ParseContext) error {
+		r, err := regexp.Compile(emailRegexString)
+		if err != nil {
+			return fmt.Errorf("compile regex: %w", err)
+		}
+
+		if !r.MatchString(*email) {
+			return errors.New("email not valid")
+		}
+
+		if len(*password) < 8 {
+			return errors.New("password should contain minimum 8 characters")
+		}
+
 		user, err := model.NewUser(*email, *password)
 		if err != nil {
 			return err
 		}
+
 		return users.Insert(context.Background(), user)
 	})
 
