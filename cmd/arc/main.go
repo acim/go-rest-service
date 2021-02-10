75c674e0783420386e305669170ae1cbe0df2a1c
Boban Acimovic <boban.acimovic@gmail.com>
2021-02-10 12:23:58 +0100

    start implementation of user subcommand

--- cmd/arc/main.go
+++ cmd/arc/main.go
@@ -43,63 +43,111 @@ type config struct {
 func main() { //nolint:funlen
 	c := &config{}
 
-	cmd := act.New("arc")
+	cmd := os.Args[1]
+	switch cmd {
+	case "serve":
+		serveCmd := act.New("serve")
 
-	if err := cmd.Parse(c, os.Args[1:]); err != nil {
-		fmt.Printf("parse arguments: %v\n", err) //nolint:forbidigo
-		os.Exit(2)                               //nolint:gomnd
-	}
+		if err := serveCmd.Parse(c, os.Args[2:]); err != nil {
+			exit("parse arguments", err)
+		}
 
-	logger, err := rest.NewLogger(c.Environment)
-	if err != nil {
-		fmt.Printf("logger: %v\n", err) //nolint:forbidigo
-		os.Exit(2)                      //nolint:gomnd
-	}
+		logger, err := rest.NewLogger(c.Environment)
+		if err != nil {
+			exit("logger", err)
+		}
 
-	db, err := pgstore.NewDB(c.Database.Hostname, c.Database.Username, c.Database.Password, c.Database.Name)
-	if err != nil {
-		logger.Error("pg connect", zap.Error(err))
-	}
+		db, err := pgstore.NewDB(c.Database.Hostname, c.Database.Username, c.Database.Password, c.Database.Name)
+		if err != nil {
+			logger.Error("pg connect", zap.Error(err))
+		}
 
-	users := pgstore.NewUsers(db, pgstore.UsersTableName("admin"))
-	jwtAuth := jwtauth.New("HS256", []byte(c.JWT.Secret), nil)
-	authController := controller.NewAuth(users, jwtAuth, logger)
+		users := pgstore.NewUsers(db, pgstore.UsersTableName("admin"))
+		jwtAuth := jwtauth.New("HS256", []byte(c.JWT.Secret), nil)
+		authController := controller.NewAuth(users, jwtAuth, logger)
 
-	mailSender := mail.NewMailgun(mailgun.NewMailgun(c.Mailgun.Domain, c.Mailgun.APIKey))
-	mailController := controller.NewMail(mailSender, c.Mailgun.Recipient, logger)
+		mailSender := mail.NewMailgun(mailgun.NewMailgun(c.Mailgun.Domain, c.Mailgun.APIKey))
+		mailController := controller.NewMail(mailSender, c.Mailgun.Recipient, logger)
 
-	router := rest.DefaultRouter(c.ServiceName, nil, logger)
-	router.Post("/auth", authController.Login)
-	router.Post("/mail", mailController.Send)
+		router := rest.DefaultRouter(c.ServiceName, nil, logger)
+		router.Post("/auth", authController.Login)
+		router.Post("/mail", mailController.Send)
 
-	router.Group(func(r chi.Router) {
-		r.Use(jwtauth.Verifier(jwtAuth))
-		r.Use(jwtauth.Authenticator)
+		router.Group(func(r chi.Router) {
+			r.Use(jwtauth.Verifier(jwtAuth))
+			r.Use(jwtauth.Authenticator)
 
-		r.Get("/auth", authController.User)
-		r.Delete("/auth", authController.Logout)
-	})
+			r.Get("/auth", authController.User)
+			r.Delete("/auth", authController.Logout)
+		})
 
-	// router.Get("/heavy", func(w http.ResponseWriter, r *http.Request) {
-	// 	err := valve.Lever(r.Context()).Open()
+		// router.Get("/heavy", func(w http.ResponseWriter, r *http.Request) {
+		// 	err := valve.Lever(r.Context()).Open()
+		// 	if err != nil {
+		// 		logger.Error("open valve lever", zap.Error(err))
+		// 	}
+		// 	defer valve.Lever(r.Context()).Close()
+
+		// 	select {
+		// 	case <-valve.Lever(r.Context()).Stop():
+		// 		logger.Info("valve closed, finishing")
+		// 	case <-time.After(2 * time.Second):
+		// 		// Do some heave lifting
+		// 		time.Sleep(2 * time.Second)
+		// 	}
+
+		// 	res := abmiddleware.ResponseFromContext(r.Context())
+		// 	res.SetPayload("all done")
+		// })
+
+		app := rest.NewServer(c.ServiceName, c.ServerPort, c.MetricsPort, router, logger)
+		app.Run()
+
+	case "user":
+		config := &struct{}{}
+		userCmd := act.New("user")
+
+		if err := userCmd.Parse(config, os.Args[2:]); err != nil {
+			exit("parse arguments", err)
+		}
+
+	// 		email := add.Arg("email", "user's email address").Required().String()
+	// password := add.Arg("password", "user's password").Required().String()
+	// add.Action(func(c *kingpin.ParseContext) error {
+	// 	if !govalidator.IsEmail(*email) {
+	// 		return errors.New("email not valid")
+	// 	}
+
+	// 	if len(*password) < 8 {
+	// 		return errors.New("password should contain minimum 8 characters")
+	// 	}
+
+	// 	user, err := model.NewUser(*email, *password)
 	// 	if err != nil {
-	// 		logger.Error("open valve lever", zap.Error(err))
-	// 	}
-	// 	defer valve.Lever(r.Context()).Close()
-
-	// 	select {
-	// 	case <-valve.Lever(r.Context()).Stop():
-	// 		logger.Info("valve closed, finishing")
-	// 	case <-time.After(2 * time.Second):
-	// 		// Do some heave lifting
-	// 		time.Sleep(2 * time.Second)
+	// 		return err
 	// 	}
 
-	// 	res := abmiddleware.ResponseFromContext(r.Context())
-	// 	res.SetPayload("all done")
+	// 	return users.Insert(context.Background(), user)
 	// })
 
-	// cmd.NewUserCmd(app, users)
-	app := rest.NewServer(c.ServiceName, c.ServerPort, c.MetricsPort, router, logger)
-	app.Run()
+	default:
+		usage()
+	}
+}
+
+func usage() {
+	usage := `Usage of arc:
+  arc <command>
+
+  Available commands:
+    serve	start rest server
+    user	create new user`
+
+	fmt.Println(usage) //nolint:forbidigo
+	os.Exit(2)         //nolint:gomnd
+}
+
+func exit(message string, err error) {
+	fmt.Printf("%s: %v\n", message, err) //nolint:forbidigo
+	os.Exit(2)                           //nolint:gomnd
 }
